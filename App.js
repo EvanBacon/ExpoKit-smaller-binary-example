4d0a20021c24f6445317e328726db6d74360dce8
Evan Bacon <baconbrix@gmail.com>
2018-12-16 00:05:23 -0800

    Removed optional modules on iOS

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Asset, GLView } from 'expo';
 export default class App extends React.Component {
   render() {
+    Asset.fromModule(require('./assets/icon.png'));
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
